3b3bb54552179608dc321665996f12a4945c6496
Dima <dima.scherbatyuk@gmail.com>
2023-02-09 18:30:32 +0200

    feature/CU-861mbrb6n: fixed read test resources for the tests.

--- net-core/src/lib.rs
+++ net-core/src/lib.rs
@@ -3,3 +3,10 @@ pub mod config;
 pub mod translator;
 pub mod transport;
 pub mod file;
+
+#[macro_export]
+macro_rules! test_resources {($fname:expr) => (
+        // The environment variable CARGO_MANIFEST_DIR provide a stable base point to reference other files.
+        // Here, we assume that there's a test/resources directory at the top level of the crate
+        concat!(env!("CARGO_MANIFEST_DIR"), "/test/resources/", $fname)
+)}
