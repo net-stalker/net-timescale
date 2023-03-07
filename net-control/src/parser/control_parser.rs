42ed70b7897a4206db18fbfd38a812c713b3f0fe
Yehor Tretiakov <tr3tiakoff@pop-os.localdomain>
2023-03-07 17:14:09 +0200

    Change the Parser. Create CommandHolder trait and add some generics. Rewrite the CLIParser code

--- net-control/src/parser/control_parser.rs
+++ net-control/src/parser/control_parser.rs
@@ -1,55 +1,60 @@
 use std::{sync::{Mutex, Once}, mem::MaybeUninit, collections::HashMap, error::Error, fmt::Debug};
 use clap::{Command, ArgMatches, command, error::ErrorKind};
 
-
-trait Parser {
-    fn parse_string_vector(&self, data: &Vec<&str>) -> Option<clap::error::Result<ArgMatches>> { None }
-    fn parse_string (&self, data: &str) -> Option<clap::error::Result<ArgMatches>> { None }
+#[allow(unused)]
+//TODO: 
+// Think on changing the HashMap<&str,...> to some abstract naming 
+// Add Error type
+trait CommandHolder{
+    fn get_commands (&self) -> Option<Result<&HashMap<&str, Command>, &str>> { None }
 }
-
-#[derive(Debug)]
-struct CLIParser{
-    config: ParserConfig
-}
-
-impl CLIParser {
-    pub fn get_cli_parser() -> &'static Mutex<Self> {
-        static mut SINGLETON: MaybeUninit<Mutex<CLIParser>> = MaybeUninit::uninit();
-        static ONCE: Once = Once::new();
+#[allow(unused)]
+trait Parser{
+    fn parse_data_vector <T, D> (t: &T, d: &Vec<D>) -> Option<clap::error::Result<ArgMatches>>
+    where 
+        T : CommandHolder, 
+        D : ToString
+    { None }
     
-        unsafe {
-            ONCE.call_once(|| {
-                SINGLETON.as_mut_ptr().write(Mutex::new(CLIParser::new()));
-            });
-        
-            &*SINGLETON.as_ptr()
-        }
-    }
-
-    fn new() -> Self {
-        CLIParser{
-            config: ParserConfig::new()
-        }
-    }
+    fn parse_data <T, D> (d: &D, ) -> Option<clap::error::Result<ArgMatches>> 
+    where 
+        T: CommandHolder,
+        D : ToString
+    { None }
 }
+
+struct CLIParser{}
 
 impl Parser for CLIParser {
-    fn parse_string_vector(&self, data: &Vec<&str>) -> Option<clap::error::Result<ArgMatches>> {
-        let service_name = data[0];
+    fn parse_data_vector <T, D> (command_holder: &T, data: &Vec<D>) -> Option<clap::error::Result<ArgMatches>>
+    where 
+        T : CommandHolder, 
+        D : ToString 
+    {
+        let data_vector: Vec<String> = data.iter().map(|x| x.to_string()).collect();
         let mut service_command: Command;
 
-        match self.config.commands.get(&service_name) {
+        let service_name: &str = data_vector[0].as_str();
+        let service_commands = command_holder.get_commands().unwrap().unwrap();
+
+        match service_commands.get(&service_name) {
             Some(command) => service_command = command.clone(),
             None => return Some(Err(clap::error::Error::new(ErrorKind::InvalidValue)))
         }
 
-        Some(service_command.try_get_matches_from_mut(data))
+        Some(service_command.try_get_matches_from_mut(data_vector))
     }
 }
 
 #[derive(Debug)]
 struct ParserConfig {
     commands: HashMap<&'static str, Command>
+}
+
+impl CommandHolder for ParserConfig {
+    fn get_commands (&self) -> Option<Result<&HashMap<&str, Command>, &str>> {
+        Some(Ok(&self.commands))
+    }
 }
 
 impl ParserConfig {
@@ -80,16 +85,8 @@ mod tests {
     use clap::Command;
 
     use super::CLIParser;
+    use super::Parser;
     use super::ParserConfig;
-
-    #[test]
-    fn expect_parser_to_be_singleton()
-    {
-        let first_parser = CLIParser::get_cli_parser();
-        let second_parser = CLIParser::get_cli_parser();
-
-        assert!(std::ptr::eq(first_parser, second_parser));
-    }
 
     #[test]
     fn parser_config_command_addition()
@@ -99,5 +96,33 @@ mod tests {
 
         config.add_command("SomeService", Command::new("SomeService"));
         assert_eq!(config.commands.len(), 1);
+    }
+
+    #[test]
+    fn should_return_error_on_parsing_with_empty_config()
+    {
+        let config = ParserConfig::new();
+
+        let data_to_parse: Vec<&str> = vec!["some", "args", "to", "parse"];
+
+        assert!(CLIParser::parse_data_vector(&config, &data_to_parse).unwrap().is_err())
+    } 
+
+    #[test]
+    fn shoud_parse_clap_example()
+    {
+        let mut config = ParserConfig::new();
+
+        let service_name = "foo";
+        let service_command = Command::new(service_name)
+        .arg(
+            clap::Arg::new("bar").short('b').action(clap::ArgAction::SetTrue)
+        );
+
+        config.add_command(service_name, service_command);
+
+        let data_to_parse: Vec<&str> = vec!["foo", "-b"];
+
+        assert!(CLIParser::parse_data_vector(&config, &data_to_parse).unwrap().is_ok())
     }
 }
