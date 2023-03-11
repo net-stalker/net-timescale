286c9a7070e236ab5d8441bd1d66300846ac8845
Dima <dima.scherbatyuk@gmail.com>
2023-03-12 00:01:50 +0200

    feature/CU-861mdcach: code polishing.

--- net-all-in-one/src/main.rs
+++ net-all-in-one/src/main.rs
@@ -4,17 +4,14 @@ use std::path::{Path, PathBuf};
 
 use log::{info, trace};
 use shaku::HasComponent;
-use syn::{Expr, Item, ItemFn, ItemTrait, parse_file, visit};
-use syn::visit::{Visit, visit_file};
-use toml::Value;
 
 use net_core::file::files::Files;
 
 fn main() {
-    let module = net_agent::module::AgentModule::builder().build();
+    let module = net_hub::module::HubModule::builder().build();
     module.resolve_ref().start();
 
-    let module = net_hub::module::HubModule::builder().build();
+    let module = net_agent::module::AgentModule::builder().build();
     module.resolve_ref().start();
 
     let module = net_timescale::module::TimescaleModule::builder().build();
