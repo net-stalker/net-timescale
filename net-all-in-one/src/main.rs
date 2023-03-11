7edc77a1ba57c2ac5a97ee59814d9b9f8ec49c88
Dima <dima.scherbatyuk@gmail.com>
2023-03-11 23:18:06 +0200

    feature/CU-861mdcach: added net-agent to all-in-one.

--- net-all-in-one/src/main.rs
+++ net-all-in-one/src/main.rs
@@ -1,55 +1,100 @@
 use std::fs::File;
 use std::io::Read;
+use std::path::{Path, PathBuf};
 
 use log::{info, trace};
+use shaku::HasComponent;
 use syn::{Expr, Item, ItemFn, ItemTrait, parse_file, visit};
 use syn::visit::{Visit, visit_file};
 use toml::Value;
-use walkdir::WalkDir;
 
 use net_core::file::files::Files;
 
 mod traits;
 
 fn main() {
-    let source_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/src/traits.rs");
-
-    // for entry in WalkDir::new(source_dir) {
-    //     let entry = entry.unwrap();
-    //     if entry.file_type().is_file() && entry.path().extension().map_or(false, |ext| ext == "rs") {
-    //         println!("{}", entry.path().display());
-    //     }
-    // }
-
-    let content = Files::read_string(source_dir);
-    let traits = count_traits(&content);
-    println!("Number of traits {:?}", traits);
-
-    let cargo = concat!(env!("CARGO_MANIFEST_DIR"), "/Cargo.toml");
-    let content = Files::read_string(cargo);
-
-    let value = content.parse::<Value>().unwrap();
-    println!("{:#?}", value);
+    let agent_module = net_agent::module::AgentModule::builder().build();
+    agent_module.resolve_ref().start();
 }
 
-fn count_traits(content: &str) -> TraitCounter {
-    let mut trait_counter = TraitCounter { count: 0 };
-    let ast = parse_file(content).unwrap();
-
-    visit_file(&mut trait_counter, &ast);
-
-    trait_counter
+fn find_sub_crates_path_with_net_prefix(toml_content: String) -> Vec<PathBuf> {
+    let toml = toml_content.parse::<Value>().unwrap();
+    toml.get("dependencies").unwrap()
+        .as_table().unwrap()
+        .iter()
+        .filter(|&x| { x.0.contains("net-") })
+        .map(|key| { key.1.get("path").unwrap() })
+        .map(|path_value| { path_value.as_str().unwrap() })
+        .map(|path_str| { PathBuf::from(path_str) })
+        .map(|path_buf| { Path::new(env!("CARGO_MANIFEST_DIR")).join(path_buf) })
+        .map(|path_buf| { path_buf.canonicalize().expect("failed to get absolute path") })
+        .collect()
 }
 
-#[derive(Debug)]
-struct TraitCounter {
-    count: usize,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl Visit<'_> for TraitCounter {
-    fn visit_item_trait(&mut self, item: &ItemTrait) {
-        self.count += 1;
-        println!("{}", item.ident);
-        visit::visit_item_trait(self, item);
+    #[test]
+    fn expected_find_sub_creates_path_with_net_prefix() {
+        let toml_content = r#"
+                                    [package]
+                                    name = "net-all-in-one"
+                                    version = "0.1.0"
+                                    edition = "2021"
+
+                                    [dependencies]
+                                    net-core = { path = "../net-core" }
+                                    net-agent = { path = "../net-agent" }
+                                    net-hub = { path = "../net-hub" }
+
+                                    log = "0.4"
+                                    quote = "1.0.23"
+                                    syn = { version = "1.0.109", features = ["parsing", "full", "visit"] }
+                                    toml = "0.7.2"
+                                    walkdir = "2.3.2"
+        "#.to_string();
+
+        let paths = find_sub_crates_path_with_net_prefix(toml_content);
+        assert_eq!(paths, vec![PathBuf::from("/Users/dmytroscherbatuik/projects/netstalker/net-monitor/net-agent"),
+                               PathBuf::from("/Users/dmytroscherbatuik/projects/netstalker/net-monitor/net-core"),
+                               PathBuf::from("/Users/dmytroscherbatuik/projects/netstalker/net-monitor/net-hub"), ]);
+    }
+
+    #[test]
+    fn test() {
+        // let cargo_path = concat!(env!("CARGO_MANIFEST_DIR"), "/Cargo.toml");
+        // let cargo_content = Files::read_string(cargo_path);
+        let toml_content = r#"
+                                    [package]
+                                    name = "net-all-in-one"
+                                    version = "0.1.0"
+                                    edition = "2021"
+
+                                    [dependencies]
+                                    net-core = { path = "../net-core" }
+                                    net-agent = { path = "../net-agent" }
+                                    net-hub = { path = "../net-hub" }
+
+                                    log = "0.4"
+                                    quote = "1.0.23"
+                                    syn = { version = "1.0.109", features = ["parsing", "full", "visit"] }
+                                    toml = "0.7.2"
+                                    walkdir = "2.3.2"
+        "#.to_string();
+
+        let dirs = find_sub_crates_path_with_net_prefix(toml_content);
+
+        // let cargo_path = concat!(env!("CARGO_MANIFEST_DIR"), "");
+        // let files = find_files(cargo_path);
+        let files = Files::find_rs_files(dirs.get(0).unwrap());
+        println!("Rust files: {:?}", files);
+
+        // files.into_iter()
+        //     .for_each(|path| {
+        //         let content = Files::read_string(path.as_str());
+        //         let traits = find_trait(&content);
+        //         println!("Number of traits {:?}", traits);
+        //     })
     }
 }
--- net-all-in-one/src/traits.rs
+++ net-all-in-one/src/traits.rs
@@ -1,3 +1,86 @@
-trait Starter {}
+use syn::{ItemImpl, ItemTrait, parse_file, Path, visit};
+use syn::visit::{Visit, visit_file};
 
-trait Handler {}
+#[derive(Debug)]
+struct TraitHunter {
+    count: usize,
+}
+
+impl TraitHunter {
+    fn find_trait(content: &str) -> TraitHunter {
+        let mut trait_counter = TraitHunter { count: 0 };
+        let ast = parse_file(content).unwrap();
+
+        visit_file(&mut trait_counter, &ast);
+
+        trait_counter
+    }
+
+    // fn find_trait_implementations(source: &str, trait_path: &Path) -> Vec<ItemImpl> {
+    //     let syntax_tree = parse_file(source).unwrap();
+    //     let mut impls = vec![];
+    //
+    //     for item in syntax_tree.items {
+    //         if let syn::Item::Impl(imp) = item {
+    //             if let Some(trait_ref) = imp.trait_.as_ref() {
+    //                 if let Some(path) = trait_ref.path.get_ident() {
+    //                     if path == &trait_path.get_ident().unwrap() {
+    //                         impls.push(imp);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     impls
+    // }
+}
+
+impl Visit<'_> for TraitHunter {
+    fn visit_item_trait(&mut self, item: &ItemTrait) {
+        self.count += 1;
+        println!("{}", item.ident);
+        visit::visit_item_trait(self, item);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use syn::parse_quote;
+    use super::*;
+
+    #[test]
+    fn expected_find_trait() {
+        let content = r#"
+                                trait Starter {}
+        "#;
+
+        let traits = TraitHunter::find_trait(content);
+        assert_eq!(traits.count, 1)
+    }
+
+    #[test]
+    fn find_trair_impl() {
+        let source = r#"
+    struct MyStruct {}
+
+    impl Debug for MyStruct {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+            unimplemented!()
+        }
+    }
+
+    impl Debug for i32 {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+            unimplemented!()
+        }
+    }
+"#;
+
+        let trait_path = parse_quote!(std::fmt::Debug);
+
+        let implementations = find_trait_implementations(source, &trait_path).unwrap();
+
+        assert_eq!(implementations.len(), 2);
+    }
+}
