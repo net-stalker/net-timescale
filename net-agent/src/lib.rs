5e6e26728d8bbc2583b1d4d9529ef6ee309b551f
Dima <dima.scherbatyuk@gmail.com>
2023-03-11 23:17:21 +0200

    feature/CU-861mdcach: use DI.

--- net-agent/src/lib.rs
+++ net-agent/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod codec;
 pub mod command;
+pub mod module;
---
+++ net-agent/src/module.rs
@@ -0,0 +1,60 @@
+use std::thread;
+
+use shaku::{Component, module};
+
+use net_core::capture;
+use net_core::config::{Config, ConfigFile, ConfigManager, ConfigSpec, FileReader};
+use net_core::starter::starter::Starter;
+use net_core::transport::connector_nng::{ConnectorNNG, Proto};
+use net_core::transport::polling::Poller;
+
+use crate::codec::Codec;
+use crate::command::dummy::DummyCommand;
+
+module! {
+    pub AgentModule {
+        components = [Agent],
+        providers = []
+    }
+}
+
+#[derive(Component)]
+#[shaku(interface = Starter)]
+pub struct Agent;
+
+impl Starter for Agent {
+    fn start(&self) {
+        let config = ConfigManager { application_name: "net-agent", file_loader: Box::new(ConfigFile) as Box<dyn FileReader> }.load();
+        // let config: String = (&self.config.dealer.endpoint).parse().unwrap();
+        let client = ConnectorNNG::builder()
+            .with_endpoint(config.dealer.endpoint)
+            .with_proto(Proto::Req)
+            .with_handler(DummyCommand)
+            .build()
+            .connect()
+            .into_inner();
+        let client_clone = client.clone();
+
+        thread::spawn(move || {
+            let capture = pcap::Capture::from_device("en0")
+                .unwrap()
+                // .promisc(true)
+                // .snaplen(65535)
+                .buffer_size(1000)
+                .open()
+                .unwrap();
+
+            let codec = Codec::new(client_clone);
+            capture::polling::Poller::new(capture)
+                .with_packet_cnt(1)
+                .with_codec(codec)
+                .poll();
+        });
+
+        thread::spawn(move || {
+            Poller::new()
+                .add(client)
+                .poll();
+        }).join().unwrap();
+    }
+}
