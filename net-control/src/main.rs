5342160ecc069214bad35102a1f0b95aca109aa6
Yehor Tretiakov <tr3tiakoff@pop-os.localdomain>
2023-03-16 18:10:56 +0200

    Add Debug logger

--- net-control/src/main.rs
+++ net-control/src/main.rs
@@ -1,5 +1,9 @@
 use net_control::server::control_server;
 
 fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
     control_server::CLIServer::new().start_server("0.0.0.0", "2222");
 }
