093422e16deb71d88328ef1b9039867413b4a076
Illia Stetsenko <illiastetsenko@knu.ua>
2023-03-23 16:59:28 +0200

    feature/CU-861md6y76: implemented ConnectionPool

--- net-timescale/src/command/connection_pool.rs
+++ net-timescale/src/command/connection_pool.rs
@@ -1,49 +1,92 @@
 use std::collections::HashSet;
 use std::sync::RwLock;
 use std::sync::Arc;
+use std::sync::atomic::AtomicU32;
+use queues::*;
 
 pub struct ConnectionPool{
     pool: Arc<RwLock<elephantry::Pool>>,
-    con_ids: Arc<RwLock<HashSet<u32>>>
+    con_queue: Arc<RwLock<Queue<u32>>>,
+    con_ids: Arc<RwLock<HashSet<u32>>>,
+    next_id: AtomicU32
 }
 pub struct PoolConnection{
-    pub con: elephantry::Connection,
+    // probably there is no need to sync the con attribute
+    pub con: Arc<RwLock<elephantry::Connection>>,
     pub con_id: u32 
 } 
 
 impl ConnectionPool {
     pub fn new(connection_string: &str, amount_of_connections: u32) -> elephantry::Result<ConnectionPool>{
         let mut pool = elephantry::Pool::new(connection_string)?;
+        let mut con_queue = Queue::<u32>::new();
         let mut con_ids = HashSet::<u32>::new();
-        
         for i in 0..amount_of_connections{
             pool = pool.add_connection(i.to_string().as_str(), connection_string)?;
+            con_queue.add(i).unwrap();
             con_ids.insert(i);
         }
         Ok(ConnectionPool {
             pool: Arc::new(RwLock::new(pool)),
-            con_ids: Arc::new(RwLock::new(con_ids))
+            con_queue: Arc::new(RwLock::new(con_queue)),
+            con_ids: Arc::new(RwLock::new(con_ids)),
+            next_id: AtomicU32::new(amount_of_connections)
         })
     }
     pub fn get_free_connection(&self) -> Result<PoolConnection, &'static str>{
-        todo!("Implement get_free_connection");
+        // TODO: add time_to_wait parameter in the method.
+        // after time_to_wait an error will be returned
+        if let Ok(id) = self.con_queue.write().unwrap().remove(){
+            if let Ok(free_pool) = self.pool.read(){
+                self.con_ids.write().unwrap().remove(&id);
+                return Ok(
+                    PoolConnection{
+                        con: Arc::new(RwLock::new(free_pool.get(id.to_string().as_str()).unwrap().clone())),
+                        con_id: id
+                    }
+                );
+            }
+            self.con_queue.write().unwrap().add(id).unwrap();
+        }
+
+        Err("No free connections")
     }
-    pub fn set_free_connection(&self, connection: PoolConnection) -> Result<(), &'static str>{
-        // I can possibly push such a connection which isn't from that connection_pool
-        // So I have to return something 
-        todo!("Implement set_free_connection"); 
+    pub fn set_free_connection(&self, connection: PoolConnection) -> Option<PoolConnection>{
+        let con_id = connection.con_id;
+        if self.con_ids.read().unwrap().contains(&con_id) {
+            return Some(connection);
+        }
+        self.con_ids.write().unwrap().insert(con_id);
+        self.con_queue.write().unwrap().add(con_id).unwrap();
+        
+        None
     }
-    pub fn add_connection(self, con_id: u32) -> elephantry::Result<ConnectionPool>{
-        todo!("Implement add_connection"); 
+    pub fn add_connection(&mut self, connection_string: &str) -> bool {
+        if let Ok(mut con_pool) = self.pool.write(){
+            
+            let temp_next_id = *self.next_id.get_mut();
+            let new_pool = con_pool.clone();
+            *con_pool = new_pool.add_connection(temp_next_id.to_string().as_str(), connection_string).unwrap();
+            
+            self.con_ids.write().unwrap().insert(temp_next_id);
+            self.con_queue.write().unwrap().add(temp_next_id).unwrap();
+            *self.next_id.get_mut() += 1;
+
+            return true;
+        } 
+
+        false
+        
     }
     pub fn get_size(&self) -> usize{
-        self.con_ids.read().unwrap().len()
+        self.con_queue.read().unwrap().size()
     }
-    // get_connection - wait until there will be a free connection or return it immidiately
 }
 #[cfg(test)]
 mod tests{
     use super::*;
+    // To check if the connections are being set 
+    // use Wireshark with filter 'tcp.port == 5432 && tcp.flags.syn == 1 && tcp.flags.ack == 0'
     #[test]
     fn new_test(){
         let pool_res = ConnectionPool::new("postgres://postgres:PsWDgxZb@localhost", 3);
@@ -73,7 +116,7 @@ mod tests{
         let free_con = pool.get_free_connection().unwrap();
         assert_eq!(pool.get_size(), 1);
 
-        assert!(pool.set_free_connection(free_con).is_ok(), "Set hasn't been a success");
+        assert!(pool.set_free_connection(free_con).is_none(), "Set hasn't been a success");
 
         assert_eq!(pool.get_size(), 2);
     }
@@ -81,11 +124,9 @@ mod tests{
     fn add_connection_test(){
         let pool_res = ConnectionPool::new("postgres://postgres:PsWDgxZb@localhost", 2);
         assert!(pool_res.is_ok(), "Connection has failed. Check if connection_string is correct or if DB is set up");
-        let pool = pool_res.unwrap();
-
-        let pool_res = pool.add_connection(2);
-        assert!(pool_res.is_ok(), "Something went wrong while adding a new connection");
-        let pool = pool_res.unwrap();
+        let mut pool = pool_res.unwrap();
+        
+        assert_eq!(pool.add_connection("postgres://postgres:PsWDgxZb@localhost"), true);
 
         assert_eq!(pool.get_size(), 3);
     }
