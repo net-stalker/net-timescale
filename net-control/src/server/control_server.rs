0cd24d7ed56801229b6a86175152485eb7be834a
Yehor Tretiakov <egorken3v@gmail.com>
2023-03-27 20:50:03 +0300

    Comment legasy code. Ready to remove it.

--- net-control/src/server/control_server.rs
+++ net-control/src/server/control_server.rs
@@ -7,9 +7,9 @@ impl <H> ControlServer<H>
 where
     H: russh::server::Handler + Default
 {
-    pub fn builder() -> ControlServerBuilder<H> {
-        ControlServerBuilder::new()
-    }
+    // pub fn builder() -> ControlServerBuilder<H> {
+    //     ControlServerBuilder::new()
+    // }
 }
 
 impl <H : russh::server::Handler + Send + Clone> russh::server::Server for ControlServer<H> {
@@ -20,29 +20,29 @@ impl <H : russh::server::Handler + Send + Clone> russh::server::Server for Contr
     }
 }
 
-pub struct ControlServerBuilder<H>
-{
-    handler: Option<Box<H>>
-}
+// pub struct ControlServerBuilder<H>
+// {
+//     handler: Option<Box<H>>
+// }
 
-impl <H> ControlServerBuilder<H> 
-where
-    H: russh::server::Handler + Default
-{
-    fn new() -> Self {
-        ControlServerBuilder {
-            handler: None
-        }
-    }
+// impl <H> ControlServerBuilder<H> 
+// where
+//     H: russh::server::Handler + Default
+// {
+//     fn new() -> Self {
+//         ControlServerBuilder {
+//             handler: None
+//         }
+//     }
 
-    pub fn with_handler(mut self, handler: H) -> Self {
-        self.handler = Some(Box::new(handler));
-        self
-    }
+//     pub fn with_handler(mut self, handler: H) -> Self {
+//         self.handler = Some(Box::new(handler));
+//         self
+//     }
 
-    pub fn build(self) -> ControlServer<H> {
-        ControlServer { 
-            handler: H::default()
-        }
-    }
-}
+//     pub fn build(self) -> ControlServer<H> {
+//         ControlServer { 
+//             handler: H::default()
+//         }
+//     }
+// }
