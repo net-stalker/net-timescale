424d57cf336e3b709b04a93e66ef614ce521c05c
Yehor Tretiakov <egorken3v@gmail.com>
2023-03-27 21:54:47 +0300

    Code polishing

--- net-control/src/main.rs
+++ net-control/src/main.rs
@@ -1,12 +1,14 @@
 use net_control::server::cli_server;
 use net_control::server::handlers::default_server_handler::DefaultServerHandler;
+use net_control::server::handlers::legasy_server_handler::ServerHandler;
 
 fn main() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Debug)
         .init();
 
-    cli_server::CLIServer::<DefaultServerHandler::default()>::builder()
+//TODO: get rid of a strange syntax
+    cli_server::CLIServer::<DefaultServerHandler>::builder()
         .build()
         .start_server();
 }
--- net-control/src/server/handlers/mod.rs
+++ net-control/src/server/handlers/mod.rs
@@ -1,1 +1,2 @@
 pub mod default_server_handler;
+pub mod legasy_server_handler;
