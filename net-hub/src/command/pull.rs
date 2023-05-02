14076b4d80d6ab1c3469544861227749ab2263f3
Yehor Tretiakov <egorken3v@gmail.com>
2023-05-02 23:19:02 +0300

    feature/CU-85zrwxwuy: net-translator now send envelope. Fix pull comment thing

--- net-hub/src/command/pull.rs
+++ net-hub/src/command/pull.rs
@@ -6,6 +6,8 @@ use simple_websockets::{Message, Responder};
 
 use net_core::transport::sockets::{Handler, Receiver, Sender};
 
+use net_timescale_api::capnp::envelope::*;
+
 pub struct PullCommand<S> {
     pub clients: Arc<RwLock<HashMap<u64, Responder>>>,
     pub db_service: Arc<S>,
@@ -15,22 +17,24 @@ impl<S: Sender> Handler for PullCommand<S> {
     fn handle(&self, receiver: &dyn Receiver, _sender: &dyn Sender) {
         let data = receiver.recv();
 
-        self.db_service.send(data);
+        let decoded_data = decode_query_data(&data);
+
+        let string_with_escapes = serde_json::json!(decoded_data);
 
         // let string_with_escapes = String::from_utf8(data).unwrap();
-        // // let unescaped_string = unescape(string_with_escapes.as_str()).unwrap();
-        // // let json_string = json!(&unescaped_string);
-        // // debug!("string with escapes: {}", string_with_escapes);
-        // // debug!("string without escapes: {}", unescaped_string);
-        // // debug!("json: {}", json_string);
-        // debug!("received from translator {:?}", string_with_escapes);
+        // let unescaped_string = unescape(string_with_escapes.as_str()).unwrap();
+        // let json_string = json!(&unescaped_string);
+        // debug!("string with escapes: {}", string_with_escapes);
+        // debug!("string without escapes: {}", unescaped_string);
+        // debug!("json: {}", json_string);
+        debug!("received from translator {:?}", string_with_escapes);
 
-        // self.clients.read().unwrap().iter().for_each(|endpoint| {
-        //     debug!("Connections: {:?}", endpoint);
-        //     let responder = endpoint.1;
-        //     responder.send(Message::Text(format!("{:?}", string_with_escapes)));
-        // });
+        self.clients.read().unwrap().iter().for_each(|endpoint| {
+            debug!("Connections: {:?}", endpoint);
+            let responder = endpoint.1;
+            responder.send(Message::Text(format!("{:?}", string_with_escapes)));
+        });
 
-        // self.db_service.send(Vec::from(string_with_escapes));
+        self.db_service.send(data);
     }
 }
