3e73019c6daffa70b4b40721f50c07e231e44fdc
Yehor Tretiakov <egorken3v@gmail.com>
2023-05-04 16:21:19 +0300

    origin/feature/CU-85zrwxwuy: Change build.rs as now we can compile all the files at ones

--- net-timescale-api/build.rs
+++ net-timescale-api/build.rs
@@ -1,10 +1,23 @@
+#[macro_export]
+macro_rules! build_schema {
+    ($file_name:expr) => {
+        ::capnpc::CompilerCommand::new()
+            .src_prefix(".capnp")
+            .file(format!("{}", $file_name))
+            .default_parent_module(vec!["capnp".into(), $file_name.into()])
+            .run()
+            .expect("Error while compiling schema");
+    }
+}
+
+fn build_capnp() {
+    let files_to_compile = std::fs::read_dir(".capnp/").unwrap();
+
+    for file_to_compile in files_to_compile {
+        build_schema!(file_to_compile.as_ref().unwrap().path().as_path().to_str().unwrap());
+    }
+}
+
 fn main() {
-    ::capnpc::CompilerCommand::new()
-        .src_prefix(".capnp")
-        .file(".capnp/network_packet.capnp")
-        .default_parent_module(vec!["capnp".into(), "network_packet".into()])
-        .file(".capnp/envelope.capnp")
-        .default_parent_module(vec!["capnp".into(), "envelope".into()])
-        .run()
-        .expect("Error while compiling schema");
+    build_capnp();
 }
