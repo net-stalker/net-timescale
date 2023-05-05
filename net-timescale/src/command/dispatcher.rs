af0aa3626910d55781acaf6af6f45fa9bf84579e
Illia Stetsenko <illiastetsenko@knu.ua>
2023-05-05 12:08:17 +0300

    feature/CU-861mj0j1x: net-timescale-api is being used now.

--- net-timescale/src/command/dispatcher.rs
+++ net-timescale/src/command/dispatcher.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
-use net_core::{transport::sockets::{Handler, Receiver, Sender}, jsons::{json_pcap_parser::JsonPcapParser, json_parser::JsonParser}};
-
-use crate::db_access::add_traffic::packet_data::PacketData;
+use net_core::{transport::sockets::{Handler, Receiver, Sender}};
+use net_timescale_api::{Decoder, api::envelope};
 
 pub struct CommandDispatcher<T>
 where T: Sender + ?Sized
@@ -20,7 +19,10 @@ where T: Sender + ?Sized
 {
     fn handle(&self, receiver: &dyn Receiver, _sender: &dyn Sender) {
         let data = receiver.recv();
-
-        self.connector.try_write().unwrap().send(&data).unwrap();
+        let envelope = envelope::Envelope::decode(data.clone());
+        let mut data = envelope.get_data().to_owned();
+        // TODO: think about adding HashMap in dispatcher with connectors to avoid such overheads
+        data.splice(0..0, envelope.get_type().as_bytes().to_owned());
+        self.consumer.send(data);
     }
 }
