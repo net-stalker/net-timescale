1b4698a7df3c420bb1a7f9e734298c3301be7d34
Illia Stetsenko <illiastetsenko@knu.ua>
2023-05-17 16:41:13 +0300

    feature/CU-85zt0y17e: fixed local connections

--- net-timescale/timescaledb/entrypoint.sh
+++ net-timescale/timescaledb/entrypoint.sh
@@ -16,8 +16,7 @@ echo "ssl_cert_file = '/var/lib/postgresql/server.crt'" >> /home/postgres/pgdata
 echo "ssl_key_file = '/var/lib/postgresql/server.key'" >> /home/postgres/pgdata/data/postgresql.conf
 
 # Modify the pg_hba.conf file to require SSL/TLS and client certificate authentication
-echo "hostssl all all 127.0.0.1/32 cert" > /home/postgres/pgdata/data/pg_hba.conf
-echo "hostssl all all ::1/128 cert" >> /home/postgres/pgdata/data/pg_hba.conf
+echo "local all postgres md5" >> /home/postgres/pgdata/data/pg_hba.conf
 echo "hostssl all all 0.0.0.0/0 cert" >> /home/postgres/pgdata/data/pg_hba.conf
 
 # Restart the PostgreSQL server to apply the new configuration
