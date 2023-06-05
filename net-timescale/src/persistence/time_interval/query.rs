6ff52fd0c68c80c3cfeb1b17e509cf0f4dfb4814
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-06-05 17:44:52 +0300

    feature/CU-861mj0j1x: moved tests from handler to query

--- net-timescale/src/persistence/time_interval/query.rs
+++ net-timescale/src/persistence/time_interval/query.rs
@@ -36,3 +36,39 @@ impl<'a> postgres_query::PostgresQuery<'a> for TimeIntervalQuery<'a> {
         (self.raw_query, &self.args)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::TimeZone;
+    use crate::persistence::postgres_query::PostgresQuery;
+
+    use super::*;
+    #[test]
+    fn select_time_interval_query_params() {
+        let start = "2020-01-01 00:00:00.000 +0000".parse::<chrono::DateTime<chrono::Utc>>().unwrap();
+        let end = "2020-01-02 00:00:00.000 +0000".parse::<chrono::DateTime<chrono::Utc>>().unwrap();
+        let select_interval_query = TimeIntervalQuery::new(&start, &end);
+        let (query, args) = select_interval_query.get_query_params();
+        assert_eq!(query,
+                   "
+                SELECT
+                    TIME_BUCKET('1 minute', \"frame_time\") AS bucket,
+                    src_addr,
+                    dst_addr
+                FROM captured_traffic
+                WHERE frame_time >= $1 AND frame_time <= $2
+                GROUP BY bucket, src_addr, dst_addr;
+            "
+        );
+        assert_eq!(format!("{:?}", args), format!("{:?}", &[&start, &end]));
+    }
+    #[test]
+    fn timestamps_from_i64_test() {
+        let start_num: i64 = 1600000000000;
+        let end_num: i64 = 1610000000000;
+        let start = Utc.timestamp_millis_opt(start_num).unwrap();
+        let end = Utc.timestamp_millis_opt(end_num).unwrap();
+        assert_eq!(start, "2020-09-13 12:26:40.000 UTC".parse::<chrono::DateTime<chrono::Utc>>().unwrap());
+        assert_eq!(end, "2021-01-07 06:13:20.000 UTC".parse::<chrono::DateTime<chrono::Utc>>().unwrap());
+    }
+}
