f74d09ac1c83eedaa7a79819e3dfb226086272fa
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-08-07 11:16:48 +0300

    feature/CU-8678ju4v4: added insert using transactions

--- net-timescale/src/repository/network_packet.rs
+++ net-timescale/src/repository/network_packet.rs
@@ -21,15 +21,32 @@ impl Into<NetworkPacket> for NetworkPacketDTO {
     }
 }
 
+const INSERT_NP_QUERY: &str =
+    "INSERT INTO CAPTURED_TRAFFIC (frame_time, src_addr, dst_addr, binary_data) VALUES ($1, $2, $3, $4)";
+
 pub async fn insert_network_packet(
     con: &Pool<Postgres>,
     packet: NetworkPacket
 ) -> Result<PgQueryResult, Error> {
-     sqlx::query("INSERT INTO CAPTURED_TRAFFIC (frame_time, src_addr, dst_addr, binary_data) VALUES ($1, $2, $3, $4)")
+     sqlx::query(INSERT_NP_QUERY)
         .bind(packet.frame_time)
         .bind(packet.src_addr)
         .bind(packet.dst_addr)
         .bind(packet.binary_data)
         .execute(con)
+        .await
+}
+
+pub async fn insert_network_packet_transaction(
+    transaction: &mut sqlx::Transaction<'_, Postgres>,
+    packet: NetworkPacket
+) -> Result<PgQueryResult, Error>
+{
+    sqlx::query(INSERT_NP_QUERY)
+        .bind(packet.frame_time)
+        .bind(packet.src_addr)
+        .bind(packet.dst_addr)
+        .bind(packet.binary_data)
+        .execute(&mut **transaction)
         .await
 }
