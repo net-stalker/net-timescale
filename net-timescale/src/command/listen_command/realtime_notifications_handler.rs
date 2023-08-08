9374e84773873a5d8aea5b32295db81b38fce95a
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-08-08 12:33:32 +0300

    feature/new-realtime-logic: finished writing code, testing is required

--- net-timescale/src/command/listen_command/realtime_notifications_handler.rs
+++ net-timescale/src/command/listen_command/realtime_notifications_handler.rs
@@ -4,11 +4,14 @@ use futures::executor::block_on;
 use net_transport::sockets::{Handler, Receiver, Sender};
 use net_proto_api::decoder_api::Decoder;
 use net_proto_api::encoder_api::Encoder;
-use sqlx::Postgres;
+use sqlx::{Encode, Postgres};
 use net_proto_api::envelope::envelope::Envelope;
 use net_timescale_api::api::network_graph::network_graph::NetworkGraphDTO;
 use crate::command::executor::PoolWrapper;
 use crate::internal_api::notification::NotificationDTO;
+use crate::repository::realtime_client;
+use crate::repository::captured_traffic;
+use crate::persistence::network_graph;
 
 pub struct RealtimeNotificationHandler<S>
 where S: Sender
@@ -31,21 +34,66 @@ where S: Sender
             connections,
         }
     }
-    async fn handle_insert_channel(&self) -> NetworkGraphDTO {
-        // 1) find minimum index from the table
-        // 2) query graph by index
-        // 3) update indexes
-        // 4) send to router
-        let mut db_connection = self.pool.get_connection().await;
-        let mut transction = db_connection.begin().await.unwrap();
+    async fn handle_insert_channel(&self) -> Option<NetworkGraphDTO>{
+        let connections = self.connections.read().unwrap();
+        let db_connection = self.pool.get_connection().await;
 
-        todo!();
+        let mut transaction = db_connection.begin().await.unwrap();
+        let index = match realtime_client::get_min_index(&mut transaction).await {
+            Ok(index) => {
+                index
+            },
+            Err(err) => {
+                log::error!("couldn't query index: {}", err);
+                return None;
+            },
+        };
+
+        let graph = match network_graph::get_network_graph_by_index(
+            &mut transaction,
+            index
+        ).await {
+            Ok(graph) => {
+                graph
+            },
+            Err(err) => {
+                log::error!("couldn't query network graph: {}", err);
+                return None;
+            }
+        };
+
+        let new_index = match captured_traffic::get_max_id(
+            &mut transaction
+        ).await {
+            Ok(new_index) => {
+                new_index
+            },
+            Err(err) => {
+                log::error!("couldn't query a new index: {}", err);
+                return None;
+            }
+        };
+        for connection in connections.iter() {
+            if let Err(err) = realtime_client::update_last_index(
+                &mut transaction,
+                *connection,
+                new_index
+            ).await {
+                log::error!("couldn't update index of {}: {}", *connection, err);
+                return None;
+            }
+        }
+        transaction.commit().await.expect("successful commit is expected");
+
+        Some(graph)
     }
 }
 impl<S> Handler for RealtimeNotificationHandler<S>
 where S: Sender
 {
     fn handle(&self, receiver: &dyn Receiver, _sender: &dyn Sender) {
+        // NOTABLE: this handler can be a dispatcher in future if we have multiple channels to handle
+        // as we have a single channel in DB that can be listened we choose simplicity over complexity
         let data = receiver.recv();
         let envelope = Envelope::decode(data.as_slice());
         let notification = NotificationDTO::decode(envelope.get_data());
@@ -53,8 +101,10 @@ where S: Sender
         match notification.get_channel() {
             "insert_channel" => {
                 log::info!("got notification from {}", notification.get_channel());
-                // 1) find minimum index from the table
-                let graph = block_on(self.handle_insert_channel()).encode();
+                let graph: Vec<u8> = if let Some(graph) = block_on(self.handle_insert_channel()) {
+                    graph.encode()
+                } else { return; };
+
                 let envelope = Envelope::new("network_graph", graph.as_slice()).encode();
                 self.router.send(envelope.as_slice());
             },
--- net-timescale/src/repository/captured_traffic.rs
+++ net-timescale/src/repository/captured_traffic.rs
@@ -5,20 +5,20 @@ use futures::stream::BoxStream;
 
 
 const GET_LAST_PACKET_ID_QUERY: &str = "
-    SELECT MAX(id)
+    SELECT MAX(id) AS index
     FROM captured_traffic;
 ";
 
 pub async fn get_max_id(
     transaction: &mut sqlx::Transaction<'_, Postgres>
-) -> Result<i32, Error>
+) -> Result<i64, Error>
 {
     let res = sqlx::query(GET_LAST_PACKET_ID_QUERY)
         .fetch_one(&mut **transaction)
         .await;
     match res {
         Ok(row) => {
-            Ok(row.get(0))
+            Ok(row.try_get::<i32, &str>("index").unwrap() as i64)
         },
         Err(err) => {
             Err(err)
