4d90f8346a6b64155d8cd4c97a82d6c480f33ea7
Illia Stetsenko <133906418+net-illia-stetsenko@users.noreply.github.com>
2023-09-29 11:37:12 +0300

    feature/CU-8692u9pjy: updated continuous aggregate (#25)
    * feature/CU-8692u9pjy: updated continuous aggregate, added index for binary_data field, updated methods for creating continuous aggregate

--- net-timescale/src/repository/continuous_aggregate.rs
+++ net-timescale/src/repository/continuous_aggregate.rs
@@ -2,25 +2,26 @@ use sqlx::{Error, Pool, Postgres};
 use sqlx::postgres::PgQueryResult;
 
 
-pub async fn create_address_pair_aggregate(con: &Pool<Postgres>)
-    -> Result<PgQueryResult, Error> {
+pub async fn create_data_aggregate(con: &Pool<Postgres>)
+                                   -> Result<PgQueryResult, Error> {
     sqlx::query("
-        CREATE MATERIALIZED VIEW address_pair_aggregate
+        CREATE MATERIALIZED VIEW data_aggregate
         WITH (timescaledb.continuous) AS
         SELECT
+            time_bucket('1 hour', frame_time) AS bucket,
             group_id,
             agent_id,
             src_addr,
             dst_addr,
-            time_bucket('1 minute', frame_time) AS bucket
+            binary_data->'l1'->'frame'->>'frame.protocols' as protocols
         FROM captured_traffic
-        GROUP BY bucket, group_id, agent_id, src_addr, dst_addr;
+        GROUP BY bucket, group_id, agent_id, src_addr, dst_addr, protocols;
     ")
         .execute(con)
         .await
 }
-pub async fn add_refresh_policy_for_address_pair_aggregate(con: &Pool<Postgres>)
-    -> Result<PgQueryResult, Error> {
+pub async fn add_refresh_policy_for_data_aggregate(con: &Pool<Postgres>)
+                                                   -> Result<PgQueryResult, Error> {
     sqlx::query(
         "SELECT add_continuous_aggregate_policy(
 	    'address_pair_aggregate',
