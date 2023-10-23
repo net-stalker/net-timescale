459f1a137354d783cd84a55b8258f3e5bdcf2c39
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-10-23 11:57:31 +0300

    feature/CU-8692vgg0t: added persistence trait for persistence structure, now we have better architecture

--- net-timescale/src/persistence/mod.rs
+++ net-timescale/src/persistence/mod.rs
@@ -1,12 +1,34 @@
 use std::rc::Rc;
 use net_proto_api::api::API;
 use net_proto_api::envelope::envelope::Envelope;
-use sqlx::Postgres;
-
+use sqlx::{Postgres, Transaction};
+use async_std::task::block_on;
 pub mod network_graph;
 
-pub trait ChartGenerator {
-    fn generate_chart(&self, transaction: &mut sqlx::Transaction<Postgres>, data: &Envelope)
-        -> Result<Rc<dyn API>, String>;
+#[async_trait::async_trait]
+pub trait Persistence {
+    type DTO: API + 'static;
+    async fn get_dto(
+        &self,
+        connection: &sqlx::Pool<Postgres>,
+        data: &Envelope
+    ) -> Result<Self::DTO, String>;
+
+    async fn transaction_get_dto(
+        &self,
+        transaction: &mut Transaction<'_, Postgres>,
+        data: &Envelope
+    ) -> Result<Self::DTO, String>;
+}
+
+pub trait ChartGenerator: Persistence {
+    fn generate_chart(&self, transaction: &mut Transaction<Postgres>, data: &Envelope)
+                      -> Result<Rc<dyn API>, String>
+    {
+        match block_on(self.transaction_get_dto(transaction, data)) {
+            Ok(dto) => Ok(Rc::new(dto)),
+            Err(err) => Err(err)
+        }
+    }
     fn get_requesting_type(&self) -> &'static str;
 }
