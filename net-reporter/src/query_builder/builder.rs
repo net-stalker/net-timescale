36663b0c83809ffd86bf7690ba508031b6626b37
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-03-04 13:26:34 +0200

    implemented wrapper for sqlx::query

--- net-reporter/src/query_builder/builder.rs
+++ net-reporter/src/query_builder/builder.rs
@@ -1,13 +1,15 @@
-use sqlx::{postgres::{PgArguments, PgRow}, query::QueryAs, Encode, FromRow, Postgres};
+use std::sync::Arc;
+
+use sqlx::{postgres::{PgArguments, PgRow}, query::QueryAs, Encode, FromRow, Pool, Postgres};
 
 pub struct QueryBuilder<'a, Response>
-    where Response: for <'r> FromRow<'r, PgRow>
+    where Response: for <'r> FromRow<'r, PgRow> + Send + Unpin
 {
     pub query: QueryAs<'a, Postgres, Response, PgArguments>,
 }
 
 impl<'a, Response> QueryBuilder<'a, Response>
-    where Response: for <'r> FromRow<'r, PgRow>
+    where Response: for <'r> FromRow<'r, PgRow> + Send + Unpin
 {
     pub fn new(initial_query: &'a str) -> Self {
         let query = sqlx::query_as(initial_query); 
@@ -35,5 +37,11 @@ impl<'a, Response> QueryBuilder<'a, Response>
             self.query = self.query.bind(value);
         }
         self
+    }
+    pub async fn execute_query(
+        self,
+        connection_pool: Arc<Pool<Postgres>>,
+     ) -> Result<Vec<Response>, sqlx::Error> {
+        self.query.fetch_all(connection_pool.as_ref()).await
     }
 }
