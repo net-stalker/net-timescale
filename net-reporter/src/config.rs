2e0ff1ddda05923c593df753cdc5f7620c4aafdd
Yehor Tretiakov <133966973+net-yehor-tretiakov@users.noreply.github.com>
2024-04-09 15:45:03 +0300

    Ns 258/update net core library version (#71)
    * NS-260/update-net-inserter: Update libs and remove unused and obsolete code.
    
    * NS-259/update-net-reporter: Update libs and remove unused and obsolete code.
    
    * NS-259/update-net-reporter: Change RequestResult. For now it is a wrap around RequestResultDTO. Nothing more. It is a bit strange though.

--- net-reporter/src/config.rs
+++ net-reporter/src/config.rs
@@ -11,12 +11,6 @@ use std::env;
 use net_config::NetConfig;
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct VerifyToken {
-    pub verify: bool,
-    pub default_token: String,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Server {
     pub host_name: String,
     pub port: String,
@@ -45,7 +39,6 @@ pub struct FusionAuthApiKey {
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, NetConfig)]
 pub struct Config {
-    pub verify_token: VerifyToken,
     pub server: Server,
     pub connection_url: ConnectionUrl,
     pub max_connection_size: MaxConnectionSize,
