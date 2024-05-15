4c58f5254f539f6198d003f38852473ec23fd0b7
Yehor Tretiakov <yehor.tretiakov@netstalker.io>
2024-05-15 14:34:05 +0300

    NS-310/add-insert-buffer: Add Traffic_Buffer to migrations

--- docker/migrations/migrations/0001_captured_traffic.sql
+++ docker/migrations/migrations/0001_captured_traffic.sql
@@ -33,3 +33,23 @@ CREATE INDEX IF NOT EXISTS Pcap_Insertion_Time_Index ON Traffic USING BRIN (Inse
 CREATE INDEX IF NOT EXISTS Pcap_Network_ID_Index ON Traffic USING HASH (Network_ID);
 CREATE INDEX IF NOT EXISTS Pcap_Tenant_ID_Index ON Traffic USING HASH (Tenant_ID);
 CREATE INDEX IF NOT EXISTS Pcap_Parsed_Data_Index ON Traffic USING GIN (Parsed_Data);
+
+CREATE TABLE IF NOT EXISTS Traffic_Buffer
+(
+    Pcap_ID             TEXT,
+    Insertion_Time      TIMESTAMPTZ NOT NULL,
+    Network_ID          TEXT,
+    Tenant_ID           TEXT NOT NULL,
+    Raw_Pcap_File_Path  TEXT NOT NULL,
+    Parsed_Data         JSONB NOT NULL,
+
+    PRIMARY KEY (Pcap_ID),
+
+    FOREIGN KEY (Network_ID) REFERENCES Networks(Network_ID)
+);
+
+CREATE INDEX IF NOT EXISTS Pcap_ID_Index ON Traffic_Buffer USING HASH (Pcap_ID);
+CREATE INDEX IF NOT EXISTS Pcap_Insertion_Time_Index ON Traffic_Buffer USING BRIN (Insertion_Time);
+CREATE INDEX IF NOT EXISTS Pcap_Network_ID_Index ON Traffic_Buffer USING HASH (Network_ID);
+CREATE INDEX IF NOT EXISTS Pcap_Tenant_ID_Index ON Traffic_Buffer USING HASH (Tenant_ID);
+CREATE INDEX IF NOT EXISTS Pcap_Parsed_Data_Index ON Traffic_Buffer USING GIN (Parsed_Data);
