390bde4012bdad441ab58b715625ef3df6d18de7
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-05-24 16:24:15 +0300

    added buffer handler

--- net-reporter/src/handlers/buffer_handlers/buffer/handler.rs
+++ net-reporter/src/handlers/buffer_handlers/buffer/handler.rs
@@ -1,53 +1,48 @@
 use std::sync::Arc;
 
 use net_component::handler::network_service_handler::NetworkServiceHandler;
+use net_reporter_api::api::buffer::buffer_request::BufferRequestDTO;
 use net_reporter_api::api::network_packet::network_packets::NetworkPacketsDTO;
-use net_reporter_api::api::network_packet::network_packets_request::NetworkPacketsRequestDTO;
 use sqlx::Error;
 use sqlx::Pool;
 use sqlx::Postgres;
 
 use net_core_api::api::envelope::envelope::Envelope;
-use net_core_api::core::decoder_api::Decoder;
 use net_core_api::core::encoder_api::Encoder;
 use net_core_api::core::typed_api::Typed;
 use sqlx::Transaction;
 
+use crate::handlers::network_packet_handlers::network_packets::response::network_packet::NetworkPacket;
 use crate::handlers::network_packet_handlers::network_packets::response::network_packets::NetworkPackets;
 
-use super::response::network_packet::NetworkPacket;
 
-const GET_NETWORK_PACKETS: &str = "
+const GET_BUFFER: &str = "
     SELECT
-        Traffic.Pcap_ID AS id,
-        Traffic.Network_Id AS network_id,
-        Traffic.Insertion_Time AS insertion_time,
-        Traffic.Parsed_Data->'l3'->'ip'->>'ip.src' AS src,
-        Traffic.Parsed_Data->'l3'->'ip'->>'ip.dst' AS dst,
+        Traffic_Buffer.Pcap_ID AS id,
+        Traffic_Buffer.Network_Id AS network_id,
+        Traffic_Buffer.Insertion_Time AS insertion_time,
+        Traffic_Buffer.Parsed_Data->'l3'->'ip'->>'ip.src' AS src,
+        Traffic_Buffer.Parsed_Data->'l3'->'ip'->>'ip.dst' AS dst,
         array_agg(string_to_array(Traffic.Parsed_Data->'l1'->'frame'->>'frame.protocols', ':')) AS protocols,
-        Traffic.Parsed_Data AS json_data
-    FROM Traffic
-    WHERE
-        (COALESCE(ARRAY_LENGTH($1, 1), 0) = 0 OR Traffic.Network_ID IN (SELECT UNNEST($1)))
-        AND Traffic.Tenant_Id = $2
-    GROUP BY Traffic.Pcap_ID;
+        Traffic_Buffer.Parsed_Data As json_data
+    FROM Traffic_Buffer
+    WHERE Traffic_Buffer.Tenant_Id = $1
+    GROUP BY Traffic_Buffer.Pcap_ID;
 ";
 
 #[derive(Default)]
-pub struct NetworkPacketsHandler {}
+pub struct BufferHandler {}
 
-impl NetworkPacketsHandler {
+impl BufferHandler {
     pub fn boxed(self) -> Box<Self> {
         Box::new(self)
     }
 
     async fn execute_query(
         transcation: &mut Transaction<'_, Postgres>,
-        network_ids: &[Option<String>],
         tenant_id: &str,
     ) -> Result<Vec<NetworkPacket>, Error> {
-        sqlx::query_as(GET_NETWORK_PACKETS)
-            .bind(network_ids)
+        sqlx::query_as(GET_BUFFER)
             .bind(tenant_id)
             .fetch_all(&mut **transcation)
             .await
@@ -55,7 +50,7 @@ impl NetworkPacketsHandler {
 }
 
 #[async_trait::async_trait]
-impl NetworkServiceHandler for NetworkPacketsHandler {
+impl NetworkServiceHandler for BufferHandler {
     async fn handle(
         &self,
         connection_pool: Arc<Pool<Postgres>>,
@@ -66,14 +61,10 @@ impl NetworkServiceHandler for NetworkPacketsHandler {
         if enveloped_request.get_type() != self.get_handler_type() {
             return Err(format!("wrong request is being received: {}", enveloped_request.get_type()).into());
         }
-        let request = NetworkPacketsRequestDTO::decode(enveloped_request.get_data());
-        // kekw moment
-        let network_ids = request.get_id();
         let mut transcaction = connection_pool.begin().await?;
 
         let executed_query_response = Self::execute_query(
             &mut transcaction,
-            network_ids,
             tenant_id,
         ).await?;
         transcaction.commit().await?;
@@ -90,6 +81,6 @@ impl NetworkServiceHandler for NetworkPacketsHandler {
     }
     
     fn get_handler_type(&self) -> String {
-        NetworkPacketsRequestDTO::get_data_type().to_string()
+        BufferRequestDTO::get_data_type().to_string()
     }
 }
--- net-reporter/src/handlers/mod.rs
+++ net-reporter/src/handlers/mod.rs
@@ -1,3 +1,4 @@
+pub mod buffer_handlers;
 pub mod chart_handlers;
 pub mod filters_handlers;
 pub mod network_handlers;
