7ba46c09f97397b292aee3da495cea0845a195ab
net-illia-stetsenko <illia.stetsenko@netstalker.io>
2024-06-18 15:28:07 +0300

    added migration files for http request methods distribution chart

---
+++ docker/migrations/migrations/20240618122037_http_request_methods_distribution.down.sql
@@ -0,0 +1,11 @@
+-- Add down migration script here
+
+-- Drop the indexes created on Http_Request_Methods_Distribution_Materialized_View
+DROP INDEX IF EXISTS idx_http_request_methods_distribution_tenant_network;
+DROP INDEX IF EXISTS idx_http_request_methods_distribution_frametime_brin;
+DROP INDEX IF EXISTS idx_http_request_methods_distribution_src_dst_ip;
+DROP INDEX IF EXISTS idx_http_request_methods_distribution_http_part;
+DROP INDEX IF EXISTS idx_http_request_methods_distribution_packet_length;
+
+-- Drop the materialized view Http_Request_Methods_Distribution_Materialized_View
+DROP MATERIALIZED VIEW IF EXISTS Http_Request_Methods_Distribution_Materialized_View;
